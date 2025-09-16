503fe0613fd7689e3c21664df10ca0336c6de5d4
RubelAhmed10082000 <ra38697@gmail.com>
2025-09-16 11:25:11 +0100

    deleting venv file

--- Frontend/venv/Scripts/activate.bat
+++ Frontend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\44785\OneDrive - OneWorkplace\Documents\Coding\FLSK\Arquivio\venv
+set VIRTUAL_ENV=D:\44785\Documents\Projects\Crag-Weather-Database\frontend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
